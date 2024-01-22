44563d7f943d02a7ad1c2724fb40bbbf403b2915
Байбаков Кирилл Дмитриевич <bkd@GosSort.local>
2024-01-22 10:33:13 +0300

    fix: task5

--- Тема 2/task5.py
+++ Тема 2/task5.py
@@ -2,7 +2,7 @@ a = input("Введите первую сторону: ")
 b = input("Введите вторую сторону: ")
 c = input("Введите третью сторону: ")
 
-if int(a) + int(b) > int(c) or int(a) + int(c) > int(b) or int(c) + int(b) > int(a):
+if int(a) + int(b) > int(c) and int(a) + int(c) > int(b) and int(c) + int(b) > int(a):
   print("Треугольник существует")
 else:
   print("Треугольник не существует")
