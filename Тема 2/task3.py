7a5dc50e655c45f35d64605a422d2dcc350a19e7
Байбаков Кирилл Дмитриевич <bkd@GosSort.local>
2024-01-22 10:26:44 +0300

    feat: task5

--- Тема 2/task3.py
+++ Тема 2/task3.py
@@ -1,10 +1,10 @@
 a = input("Введите первую сторону: ")
 b = input("Введите вторую сторону: ")
-с = input("Введите третью сторону: ")
+c = input("Введите третью сторону: ")
 
-if int(a) == int(b) == int(с):
+if int(a) == int(b) == int(c):
   print("Равносторонний")
-elif int(a) == int(b) or int(a) == int(с) or int(с) == int(b):
+elif int(a) == int(b) or int(a) == int(c) or int(c) == int(b):
   print("Равнобедренный")
 else:
   print("Разносторонний")
